94919242a4ee1d3bdd49a4f12fca02e954eaf8b9
Vladimir Bajenov <vladimir@bajenov.info>
2025-07-08 17:15:18 +0400

    Add ContentLanguages facade

---
+++ src/Facades/ContentLanguages.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MountainClans\LivewireSelect\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see \MountainClans\LivewireTranslatable\Services\ContentLanguages
+ */
+class ContentLanguages extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return \MountainClans\LivewireTranslatable\Services\ContentLanguages::class;
+    }
+}
