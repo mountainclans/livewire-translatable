db918811957ebda480846ad57938a52d1609cc5e
Vladimir Bajenov <vladimir@bajenov.info>
2025-07-08 14:55:59 +0400

    Initial config

---
+++ config/livewire-translatable.php
@@ -0,0 +1,6 @@
+<?php
+
+// config for MountainClans/LivewireTranslatable
+return [
+
+];
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Database\Factories;
+namespace MountainClans\LivewireTranslatable\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
