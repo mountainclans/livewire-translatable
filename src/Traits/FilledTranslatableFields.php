4d5681d89ca1753d0d5e76daab7417962ce5226f
Vladimir Bajenov <vladimir@bajenov.info>
2025-09-29 16:41:43 +0400

    Fix translation errors

--- src/Traits/FilledTranslatableFields.php
+++ src/Traits/FilledTranslatableFields.php
@@ -3,12 +3,17 @@
 namespace MountainClans\LivewireTranslatable\Traits;
 
 use MountainClans\LivewireTranslatable\Services\ContentLanguages;
+use Throwable;
 
 trait FilledTranslatableFields
 {
     public function getAllTranslations(string $key)
     {
-        $source = $this->getTranslations($key);
+        try {
+            $source = $this->getTranslations($key);
+        } catch (Throwable $exception) {
+            $source = [];
+        }
 
         foreach (ContentLanguages::all() as $languageKey => $language) {
             if (!isset($source[$languageKey])) {
